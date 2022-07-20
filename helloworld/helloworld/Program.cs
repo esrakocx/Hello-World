ea43e46b1768b9ffea9d6df65264aac7906bda1c
Esra Koc <eocesra@gmail.com>
2022-07-20 21:04:51 +0300

    second code

--- helloworld/helloworld/Program.cs
+++ helloworld/helloworld/Program.cs
@@ -11,6 +11,7 @@ namespace helloworld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I came!");
         }
     }
 }
